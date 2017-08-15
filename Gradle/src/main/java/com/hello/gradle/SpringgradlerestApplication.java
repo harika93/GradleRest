faa5ccd5674d6400598bba4c1f0d1d0bee5ad1fc
harika93 <30352059+harika93@users.noreply.github.com>
2017-08-15 17:12:11 -0500

    Add files via upload

---
+++ Gradle/src/main/java/com/hello/gradle/SpringgradlerestApplication.java
@@ -0,0 +1,13 @@
+package com.hello.gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgradlerestApplication {
+
+    public static void main(String[] args) {
+            SpringApplication.run(SpringgradlerestApplication.class, args);
+        }
+    }
+
